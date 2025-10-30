292b9609f8ebc4fac285c802e42f6bb35c995af8
uevertinho <devvertinho@gmail.com>
2025-10-30 16:01:07 -0300

    programação funcional - sort

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,10 +3,15 @@ package application;
 import entities.Product;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Program {
+
+    //função que recebe dois produtos e retorna a comparação de preço deles
+    public static int compareProducts(Product p1, Product p2){
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
+
     public static void main(String[] args) {
         List<Product> list = new ArrayList<>();
 
@@ -14,16 +19,17 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        // A expressão lambda (p -> p.getName().toUpperCase()) indica que
-        // para cada objeto Product 'p', o critério de comparação será
-        // o nome em letras maiúsculas. O Comparator.comparing recebe essa
-        // função e a utiliza para ordenar a lista alfabeticamente
-        // ignorando diferenças entre maiúsculas e minúsculas.
+        //a chamada do metodo 'sort' que ordena a lista
+        //eu posso passar a referência para função compareProducts
+        // '::' é a sintaxe do java
+        //na programação funcional, eu posso passar
+        //uma função como argumento de outra função
+        //isso significa eu falar que uma função
+        //é um objeto de primeira classe ou primeira ordem
+        //ele pode ser passado como argumento para outras funções
+        //e ele pode ainda ser retornado por uma função.
+        list.sort(Program::compareProducts);
 
-        list.sort(Comparator.comparing(p -> p.getName().toUpperCase()));
-
-        for (Product p : list){
-            System.out.println(p);
-        }
+        list.forEach(System.out::println);
     }
 }
